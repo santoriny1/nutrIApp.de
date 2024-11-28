63755161a8bd874d31df9707ba015b63c797f6ce
David Zavala Moreno <dzavala@protossecurity.com>
2024-11-28 00:34:44 -0600

    create project template

---
+++ nutrIApp.de/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace nutrIApp.de;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ nutrIApp.de/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace nutrIApp.de;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
